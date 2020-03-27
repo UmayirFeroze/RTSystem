d247757223319fcf0b7f6e5aabdf02fdf36529d3
Umair Feroze <umayir10@gmail.com>
2020-03-27 16:35:38 +0530

    Updated StartUp.cs

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,7 @@ namespace RTSystem
             });
 
             services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IBuyerBidService, BuyerBidService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
