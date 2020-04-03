cf26003e6ca601bd024d50d49e053bbeaa3d0d4d
Umayir Feroze <52625821+UmayirFeroze@users.noreply.github.com>
2020-04-03 19:33:21 +0530

    db conn

---
+++ Data/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RTSystem.Data
+{
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+        public int? BuyerBidId { get; set; }
+        public int? SellerBidId { get; set; }
+
+        public virtual BuyerBid BuyerBid { get; set; }
+        public virtual SellerBid SellerBid { get; set; }
+    }
+}
---
+++ Data/Models/Users.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RTSystem.Data
+{
+    public class Users
+    {
+        [Required] public int userId { get; set; }
+
+        [Required] public string firstName { get; set; }
+
+        [Required] public string lastName { get; set; }
+
+        [Required] public string phone { get; set; }
+
+        [Required] [EmailAddress] public string email { get; set; }
+
+        [Required] public string password { get; set; }
+
+        [Required] public string businessName { get; set; }
+
+        public string businessDescription { get; set; }
+
+        public string businessPhone { get; set; }
+
+        [Required] public string businessAddress { get; set; }
+
+        [Required] public string businessType { get; set; }
+
+    }
+}
