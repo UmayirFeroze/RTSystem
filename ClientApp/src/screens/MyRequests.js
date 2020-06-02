855bc20e496fd69120b0ccc5e5f3b665dbd51116
Umair Feroze <umayir10@gmail.com>
2020-06-03 02:53:37 +0530

    My Requests Header Fixed

--- ClientApp/src/screens/MyRequests.js
+++ ClientApp/src/screens/MyRequests.js
@@ -184,7 +184,7 @@ export class MyRequests extends Component {
       <div className="themePage">
         <Header />
         <Navbar />
-        <h1 style={{ paddingLeft: 10 }}>My Requested Bids</h1>
+        <h1 style={{ paddingLeft: 10 }}>My Requested</h1>
         <div className="container">
           <div className="sideNav">
             <button name="allBids" value="allBids" onClick={this.handleClick}>
